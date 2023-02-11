f405de14bec614bcc6dc97852b844a08bb424ecb
Joseph Nevado <josephnevado12@gmail.com>
2023-02-11 12:36:15 -0800

    initial files

---
+++ B-Line/Core/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  B-Line
+//
+//  Created by Joseph Nevado on 2023-02-11.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
