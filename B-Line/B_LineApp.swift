365fda5ea9fd115ecebf7b4296d1a5593c2c4896
Joseph Nevado <josephnevado12@gmail.com>
2023-02-11 12:29:38 -0800

    Initial Commit

---
+++ B-Line/B_LineApp.swift
@@ -0,0 +1,17 @@
+//
+//  B_LineApp.swift
+//  B-Line
+//
+//  Created by Joseph Nevado on 2023-02-11.
+//
+
+import SwiftUI
+
+@main
+struct B_LineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
